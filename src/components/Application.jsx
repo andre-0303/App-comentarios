28662b7dd6b142fda7ab632edba25d5cb1b8e168
andre-0303 <bandeiradev.ti@gmail.com>
2025-03-15 17:20:50 -0300

    Primeiras configurações

---
+++ src/components/Application.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import People from '../assets/Consulting_Isometric 1.png';
+
+const Application = () => {
+    return ( 
+       <>
+        <div>
+            <img src={People} alt="People" />
+        </div>
+       </> 
+     );
+}
+ 
+export default Application;
