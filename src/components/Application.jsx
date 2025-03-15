b9adcaf412065750ce0c3ae43a60a27490daf3ce
andre-0303 <bandeiradev.ti@gmail.com>
2025-03-15 17:26:33 -0300

    Criação do Application.css e funções de Click e Change

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import People from '../assets/Consulting_Isometric 1.png';
+import './Application.css';
 
 const Application = () => {
+    function Clique() {
+        console.log('Clicou');
+    }
+
+    function writeInTextArea(event) {
+        console.log(event.target.value);
+    }
+
     return ( 
        <>
         <div>
             <img src={People} alt="People" />
+            <textarea onChange={writeInTextArea}></textarea>
+            <button onClick={Clique}>Comentar</button>
+
+            <ul>
+                <li>Item</li>
+            </ul>
         </div>
        </> 
      );
