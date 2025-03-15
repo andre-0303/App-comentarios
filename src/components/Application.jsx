231ab3a8fa44b9f350c49e815fabbb900d293ef5
andre-0303 <bandeiradev.ti@gmail.com>
2025-03-15 17:45:47 -0300

    Estilização e lógica

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,29 +1,50 @@
-import React from "react";
-import People from '../assets/Consulting_Isometric 1.png';
-import './Application.css';
+import React, { useState } from "react";
+import People from "../assets/Consulting_Isometric 1.png";
 
 const Application = () => {
-    function Clique() {
-        console.log('Clicou');
+  const [comentario, setComentario] = useState("");
+  const [comentarios, setComentarios] = useState([]);
+
+  function writeInTextArea(event) {
+    setComentario(event.target.value);
+  }
+
+  function Clique() {
+    if (comentario.trim() !== "") {
+      setComentarios([...comentarios, comentario]);
+      setComentario("");
     }
+  }
 
-    function writeInTextArea(event) {
-        console.log(event.target.value);
-    }
+  return (
+    <div className="container mt-5 d-flex flex-column align-items-center">
+      {/* Imagem */}
+      <img src={People} alt="People" className="img-fluid mb-4 rounded shadow" style={{ maxWidth: "300px" }} />
 
-    return ( 
-       <>
-        <div>
-            <img src={People} alt="People" />
-            <textarea onChange={writeInTextArea}></textarea>
-            <button onClick={Clique}>Comentar</button>
+      {/* Área de Comentário */}
+      <textarea
+        className="form-control mb-3 w-75"
+        rows="3"
+        placeholder="Escreva seu comentário..."
+        value={comentario}
+        onChange={writeInTextArea}
+      ></textarea>
 
-            <ul>
-                <li>Item</li>
-            </ul>
-        </div>
-       </> 
-     );
-}
- 
+      {/* Botão */}
+      <button className="btn btn-primary mb-3" onClick={Clique}>
+        Comentar
+      </button>
+
+      {/* Lista de Comentários */}
+      <ul className="list-group w-75">
+        {comentarios.map((coment, index) => (
+          <li key={index} className="list-group-item">
+            {coment}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 export default Application;
